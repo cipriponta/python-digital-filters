ff017980a7901413df5412d39b445c1b04c8f04b
Ciprian Ponta <ponta_ciprian@yahoo.com>
2023-12-31 02:55:12 +0200

    The UI now displays the graph and plays the samples (original and low passed version)

--- digital_filter.py
+++ digital_filter.py
@@ -2,76 +2,140 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 import time
+import sys
+from PyQt5 import QtWidgets, QtCore
+import pyqtgraph as pg
+from main_window import Ui_MainWindow
+import pyaudio
+import wave
 
-# TODO: Architecture:   - UI that opens a sample from a song which allows you to low pass it
-# TODO:                 - The UI contains a knob which allows us to change the cutoff frequency and displays the updated signal after 2 seconds (debouncing)
-# TODO:                 - The UI should allow us to play the original song and the low passed song
-# TODO: Figure ouy how to use PyQT and how to plot using PyQT # https://www.youtube.com/watch?v=je9Cj7G5pnY. If this doesn't work fall back to jupyter notebooks? 
+class UITask(QtCore.QRunnable):
+    def __init__(self, task):
+        super().__init__()
+        self.task = task
 
-def apply_filter(b, a, inputs):
-    outputs = np.zeros(inputs.size)
+    def run(self):
+        self.task()
 
-    for input_index in range(0, inputs.size):
-        # print("y[{0}]=".format(input_index), end="")
-        for b_index in range(0, b.size):
-            if input_index - b_index >= 0:
-                # print("+b[{0}]x[{1}]=".format(b_index, input_index - b_index), end="")
-                outputs[input_index] = outputs[input_index] + b[b_index] * inputs[input_index - b_index]
-        for a_index in range(1, a.size):
-            if input_index - a_index >= 0:
-                # print("-a[{0}]y[{1}]=".format(a_index, input_index - a_index), end="")
-                outputs[input_index] = outputs[input_index] - a[a_index] * outputs[input_index - a_index]
-        # print()
-    return outputs
 
-def main_loop():
-    sample_period = 0.1
-    sample_freq = 1 / sample_period
-    cutoff_freq = 0.4
-    samples = np.arange(0, 100, 0.1, dtype=float)
+class DigitalFilter_UI(Ui_MainWindow):
+    def setupUi(self, MainWindow):
+        super().setupUi(MainWindow)
 
-    original_signal = np.sin(0.08 * samples) + np.sin(0.2 * samples) + np.sin(0.32 * samples) + np.sin(0.4 * samples)
-    signal_with_noise = original_signal + np.random.rand(original_signal.size) - np.random.rand(original_signal.size)
-    b, a = sp.signal.butter(3, cutoff_freq/sample_freq)
+        self.graphicsView = pg.GraphicsLayoutWidget(self.centralwidget)
+        self.graphicsView.setGeometry(QtCore.QRect(220, 10, 731, 541))
+        self.graphicsView.setObjectName("graphicsView")
+        self.firstSubplot = self.graphicsView.addPlot(row = 0, col = 0)
+        self.secondSubplot = self.graphicsView.addPlot(row = 1, col = 0)
 
-    plt.ion()
-    figure = plt.figure()
-    subplot = figure.add_subplot(1, 1, 1)
-    graph_original_signal, graph_signal_with_noise, graph_signal_filtered = subplot.plot(samples, original_signal, 
-                                                                                         samples, original_signal, 
-                                                                                         samples, original_signal)
+        self.pushButtonDrawGraph.setEnabled(False)
+        self.pushButtonLowPassSound.setEnabled(False)
 
-    while True:
-        signal_with_noise = original_signal + np.random.rand(original_signal.size) - np.random.rand(original_signal.size)
-        filtered_signal = apply_filter(b, a , signal_with_noise)
-        graph_signal_with_noise.set_ydata(signal_with_noise)
-        graph_signal_filtered.set_ydata(filtered_signal)
-        figure.canvas.draw()
-        figure.canvas.flush_events()
-        time.sleep(1)
+        self.horizontalSliderCutOffFreq.valueChanged.connect(self.cutOffFreqSliderValueChangedEvent)
+        self.pushButtonDrawGraph.clicked.connect(self.drawButtonEvent)
+        self.pushButtonOriginalSound.clicked.connect(self.playSampleOriginal)
+        self.pushButtonLowPassSound.clicked.connect(self.playSampleLowPass)
 
-def read_audio_sample():
-    sample_rate, sample_array = sp.io.wavfile.read("MusicSoundsBetterWithYouSample.wav")
-    sample_first_channel = sample_array[:, 0]
-    sample_second_channel = sample_array[:, 1]
+        self.threadpool = QtCore.QThreadPool()
 
-    cutoff_freq = 1000
-    b, a = sp.signal.butter(6, cutoff_freq / sample_rate)
-    low_pass_first_channel = apply_filter(b, a, sample_first_channel)
-    low_pass_second_channel = apply_filter(b, a, sample_second_channel)
-    low_pass_sample_array = np.stack((low_pass_first_channel, low_pass_second_channel), axis=-1)
-    low_pass_sample_array = np.asarray(low_pass_sample_array, dtype=np.int16)
-    sp.io.wavfile.write("MusicSoundsBetterWithYouLowPass.wav", sample_rate, low_pass_sample_array)
+        self.sample_rate = None
+        self.sample_array = None
+        self.low_pass_sample_array = None
 
-    figure = plt.figure()
-    subplot_first_channel = figure.add_subplot(2, 1, 1)
-    _ = subplot_first_channel.plot(sample_first_channel, 'b', low_pass_first_channel, 'r')
-    subplot_second_channel = figure.add_subplot(2, 1, 2)
-    _ = subplot_second_channel.plot(sample_second_channel, 'b', low_pass_second_channel, 'r')
-    figure.canvas.draw()
-    figure.canvas.flush_events()
+    def cutOffFreqSliderValueChangedEvent(self):
+        slider_value = int(self.horizontalSliderCutOffFreq.value())
+        if slider_value > 0:
+            self.pushButtonDrawGraph.setEnabled(True)
+        else:
+            self.pushButtonDrawGraph.setEnabled(False)
+        self.labelCutOffFreqValue.setText(str(int((slider_value / 100) * 44800)))
 
-    plt.show()
+    def drawButtonEvent(self):
+        self.threadpool.start(UITask(self.drawFilteredAudioSampleTask))
+
+    def drawFilteredAudioSampleTask(self): 
+        self.pushButtonOriginalSound.setEnabled(False)
+        self.pushButtonLowPassSound.setEnabled(False)
+        
+        try:
+            cutoff_freq = int(float(self.labelCutOffFreqValue.text()))
+            self.filterAudioSample(cutoff_freq)
+            self.firstSubplot.plot(self.sample_array[:, 0], pen = pg.mkPen('b'))
+            self.firstSubplot.plot(self.low_pass_sample_array[:, 0], pen = pg.mkPen('r'))
+            self.secondSubplot.plot(self.sample_array[:, 1], pen = pg.mkPen('b'))
+            self.secondSubplot.plot(self.low_pass_sample_array[:, 1], pen = pg.mkPen('r'))
+        except Exception as exception:
+            print("Exception: ", str(exception))
+
+        self.pushButtonOriginalSound.setEnabled(True)
+        self.pushButtonLowPassSound.setEnabled(True)
+
+    def filterAudioSample(self, cutoff_freq):
+        self.sample_rate, self.sample_array = sp.io.wavfile.read("MusicSoundsBetterWithYouSample.wav")
+
+        sample_first_channel = self.sample_array[:, 0]
+        sample_second_channel = self.sample_array[:, 1]
+
+        b, a = sp.signal.butter(6, cutoff_freq / self.sample_rate)
+        low_pass_first_channel = self.applyFilter(b, a, sample_first_channel)
+        low_pass_second_channel = self.applyFilter(b, a, sample_second_channel)
+        self.low_pass_sample_array = np.stack((low_pass_first_channel, low_pass_second_channel), axis=-1)
+        self.low_pass_sample_array = np.asarray(self.low_pass_sample_array, dtype=np.int16)
+        sp.io.wavfile.write("MusicSoundsBetterWithYouLowPass.wav", self.sample_rate, self.low_pass_sample_array)
+
+    def applyFilter(self, b, a, inputs):
+        outputs = np.zeros(inputs.size)
+
+        for input_index in range(0, inputs.size):
+            # print("y[{0}]=".format(input_index), end="")
+            for b_index in range(0, b.size):
+                if input_index - b_index >= 0:
+                    # print("+b[{0}]x[{1}]=".format(b_index, input_index - b_index), end="")
+                    outputs[input_index] = outputs[input_index] + b[b_index] * inputs[input_index - b_index]
+            for a_index in range(1, a.size):
+                if input_index - a_index >= 0:
+                    # print("-a[{0}]y[{1}]=".format(a_index, input_index - a_index), end="")
+                    outputs[input_index] = outputs[input_index] - a[a_index] * outputs[input_index - a_index]
+            # print()
+        return outputs
+
+    def playSample(self, sample_path):
+        chunk = 1024
+
+        sample_player = pyaudio.PyAudio()
+        sample_audio = wave.open(sample_path)
+
+        stream = sample_player.open(format = sample_player.get_format_from_width(sample_audio.getsampwidth()),
+                                    channels = sample_audio.getnchannels(),
+                                    rate = sample_audio.getframerate(),
+                                    output = True)
+        
+        data = sample_audio.readframes(chunk)
+        while data:
+            stream.write(data)
+            data = sample_audio.readframes(chunk)
+
+        sample_audio.close()
+        stream.stop_stream()
+        stream.close()
+        sample_player.terminate()
+
+    def playSampleOriginalTask(self):
+        self.playSample("MusicSoundsBetterWithYouSample.wav")
+
+    def playSampleOriginal(self):
+        self.threadpool.start(UITask(self.playSampleOriginalTask))
+
+    def playSampleLowPassTask(self):
+        self.playSample("MusicSoundsBetterWithYouLowPass.wav")
+
+    def playSampleLowPass(self):
+        self.threadpool.start(UITask(self.playSampleLowPassTask))
 
 if __name__ == "__main__":
-    read_audio_sample()
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = DigitalFilter_UI()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
